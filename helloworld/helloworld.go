1bf76e237f270a829f136f8d88d288b4c2ac585f
David Reiss <david@temporal.io>
2022-05-09 18:17:25 -0700

    even simpler

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -6,27 +6,23 @@ import (
 	"go.temporal.io/sdk/workflow"
 )
 
-type params struct {
-	Val1, Val2 int
-}
-
-var defaultParams = params{12, 19}
+var defaultNumber = 19
 
 // Workflow is a Hello World workflow definition.
 func Workflow(ctx workflow.Context, name string) (string, error) {
 	logger := workflow.GetLogger(ctx)
 	logger.Info("HelloWorld workflow started", "name", name)
 
-	// load default params
-	get := func(ctx workflow.Context) interface{} { return defaultParams }
-	eq := func(a, b interface{}) bool { return a.(params) == b.(params) }
-	var params params
-	if err := workflow.MutableSideEffect(ctx, "defaultParams", get, eq).Get(&params); err != nil {
-		panic("can't decode params:" + err.Error())
+	// load default number
+	get := func(ctx workflow.Context) interface{} { return defaultNumber }
+	eq := func(a, b interface{}) bool { return a.(int) == b.(int) }
+	var number int
+	if err := workflow.MutableSideEffect(ctx, "defaultNumber", get, eq).Get(&number); err != nil {
+		panic("can't decode number:" + err.Error())
 	}
 
 	for i := 0; i < 100; i++ {
-		logger.Info("look at my params", "val1", params.Val1, "val2", params.Val2)
+		logger.Info("look at my number", "number", number)
 		workflow.Sleep(ctx, 3*time.Second)
 	}
 
